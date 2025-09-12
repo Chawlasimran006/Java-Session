e703df12c3410a6d767264596960e6da303b5625
Chawlasimran006 <chawlasimran64@gmail.com>
2025-09-12 15:39:39 +0530

    Added Work of Day40

---
+++ Day40/NumberPattern.java
@@ -0,0 +1,15 @@
+package com.Simran715.Day40;
+
+public class NumberPattern {
+    public static void main(String []args){
+        int rows=5;
+        for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print((j+1) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
