325b27e573e79ed83f7a848ca75df29aee0d7147
Chawlasimran006 <chawlasimran64@gmail.com>
2025-09-16 13:32:57 +0530

    Update PrintFirst_LastRow.java

--- Java Session/src/com/Simran715/Day42/PrintFirst_LastRow.java
+++ Java Session/src/com/Simran715/Day42/PrintFirst_LastRow.java
@@ -1,15 +1,40 @@
-package Day42;
-
+package com.Simran715.Day42;
+import java.util.Scanner;
 public class PrintFirst_LastRow {
     public static void main(String[] args) {
-        int[][] matrix = {
-                {1, 2, 3, 4},
-                {5, 6, 7, 8},
-                {9, 10, 11, 12}
-        };
-        for (int i = 0; i < matrix.length; i++) {
-            
+        Scanner sc=new Scanner (System.in);
 
+        System.out.print("Enter number of rows: ");
+        int rows= sc.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int cols= sc.nextInt();
+
+        int [][] matrix= new int [rows][cols];
+
+        System.out.print("Enter elements of matrix: ");
+        for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<cols;j++)
+            {
+                matrix[i][j]=sc.nextInt();
+            }
         }
+
+        System.out.println("Displaying Elements of First Row:");
+        for(int j=0;j<cols;j++)
+        {
+            System.out.print(matrix[0][j]+" ");
+        }
+        System.out.println();
+
+        System.out.println("Displaying Elements of Last Row:");
+        for(int j=0;j<cols;j++)
+        {
+            System.out.print(matrix[rows-1][j]+" ");
+        }
+
+
+        sc.close();
     }
 }
