fac299fb2136e8c7f984784c88efeccba5769979
Chawlasimran006 <chawlasimran64@gmail.com>
2025-09-16 11:52:59 +0530

    Update PrintElementsOf_TwoDArray.java

--- Java Session/src/com/Simran715/Day42/PrintElementsOf_TwoDArray.java
+++ Java Session/src/com/Simran715/Day42/PrintElementsOf_TwoDArray.java
@@ -1,23 +1,38 @@
-package Day42;
+package com.Simran715.Day42;
+
+import java.util.Scanner;
 
 public class PrintElementsOf_TwoDArray {
-    public static void main(String[] args) {
-        int [] [] matrix= {
-                {1, 2, 3, 4},
-                {5, 6, 7, 8},
-                {9, 10, 11, 12}
-        };
-        for(int i=0;i<matrix.length;i++)
-        {
-            int []row=matrix[i];
-            for(int j=0;j<row.length;j++)
+
+        public static void main(String[] args) {
+            Scanner sc=new Scanner (System.in);
+
+            System.out.print("Enter number of rows: ");
+            int rows= sc.nextInt();
+
+            System.out.print("Enter number of columns: ");
+            int cols= sc.nextInt();
+
+            int [][] matrix= new int [rows][cols];
+
+            System.out.print("Enter elements of matrix: ");
+            for(int i=0;i<rows;i++)
             {
-                System.out.print(row[j]+" ");
+                for(int j=0;j<cols;j++)
+                {
+                    matrix[i][j]=sc.nextInt();
+                }
             }
-            System.out.println();
+
+            System.out.println("Displaying Elements");
+            for (int i = 0; i < rows; i++) {
+                for(int j=0;j<cols;j++){
+                    System.out.print(matrix[i][j]+" ");
+                }
+                System.out.println();
+            }
+            sc.close();
         }
-
-
-
     }
-}
+
+
